0210c1961aedcd15e65d7698a989eda4c5b5acee
Abhaypratap616 <114880762+Abhaypratap616@users.noreply.github.com>
2024-01-10 15:35:24 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useEffect, useState } from 'react'
+import './App.css'
 
-function App() {
+export default function App() {
+  const[length ,setlength]= useState("6");
+  const[numberallowed,setnumberallowed]= useState(false);
+  const[spcharallowed,setspcharallowed]= useState(false);
+  const[password,setpassword]= useState("");
+
+  const passwordgenerator = useCallback(()=>{
+    let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    if(numberallowed) charset+="0123456789";
+    if(spcharallowed) charset+="!`@#*&^%$~()?/\\><{}[]";
+    let passwordmake = "";
+    for (let i = 0; i <=length; i++) {
+      const randomIndex = (Math.floor(Math.random() * charset.length)+1);
+      passwordmake += charset.charAt(randomIndex);
+    }
+    setpassword(passwordmake);
+
+
+  },[length,numberallowed,spcharallowed]);
+
+  const copybutton =()=>{
+    navigator.clipboard.writeText(password);
+  }
+
+useEffect(()=>{passwordgenerator()},[length,numberallowed,spcharallowed,passwordgenerator]);  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div>
 
-export default App;
+    <div>
+      <h2 className='heading'>Password-Generator-System</h2>
+    </div>
+    <div className="input">
+      <input type="text" value={password} placeholder='Password' />
+      <button className='btn-1' onClick={copybutton} >Copy</button>
+    </div>
+    <div className="range">
+      <input type="range" min={6} max={20} value={length} className='under-range-1' onChange={(e)=>{setlength(e.target.value)}} />
+      <label>Lenght:{length}</label>
+    </div>
+    <div className="checkbok">
+      <input type = "checkbox"  defaultChecked={numberallowed} className='under-range-2' onChange={()=>{setnumberallowed((prev)=>!prev)}}/>
+    <label>Numbers</label>
+    <input type = "checkbox"  defaultChecked={spcharallowed} onChange={()=>{setspcharallowed((prev)=>!prev)}}/>
+    <label>specialcharallowed</label>
+
+
+    </div>
+
+
+
+    </div>
+    
+  )
+}
